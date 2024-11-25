05d1747ccf6ec15b5d45c0b8e1fc68d485b151a9
Hieu <lycapower2002@gmail.com>
2024-11-25 16:45:49 +0700

    feat: gen schema

--- dbfiddle_api.py
+++ dbfiddle_api.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 import pandas as pd
 from gen_data import gen_data
+from gen_schema import gen_schema
 
 app = FastAPI()
 
 
 @app.post("/gen_data")
 async def generate_data(json_data: dict) -> dict:
-    return gen_data(json_data, list_output=True)
+    result = gen_data(json_data, list_output=True)
+    return jsonable_encoder(result)
+
+
+@app.post("/gen_schema")
+async def generate_schema(json_data: dict) -> str:
+    return gen_schema(json_data['query'])
