ab5b6213fe6aea373f2a40f425ae0e62f2fcb1cb
Hieu <lycapower2002@gmail.com>
2024-11-26 15:16:18 +0700

    fix: json_dbml is str/json

--- dbfiddle_api.py
+++ dbfiddle_api.py
@@ -3,14 +3,19 @@ from fastapi.encoders import jsonable_encoder
 import pandas as pd
 from gen_data import gen_data
 from gen_schema import gen_schema
+import json
 
 app = FastAPI()
 
 
 @app.post("/gen_data")
 async def generate_data(json_data: dict) -> dict:
+    try:
+        dbml_json=json.loads(json_data["dbml_json"])
+    except:
+        dbml_json = json_data["dbml_json"]
     result = gen_data(
-        json_data["dbml_json"],
+        dbml_json,
         list_output=True,
         ai_data=True,
         custom_prompt=json_data["custom_prompt"],
